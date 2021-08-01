e9fc1983440eaac89d78d2c2a4e16e1da0f10fe4
astevenaa <astevenaa@gmail.com>
2021-08-01 14:52:42 -0700

    added components

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Headed";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcome to class</h1>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Headed.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Headed = () => {
+  return <header>header</header>;
+};
+
+export default Headed;
