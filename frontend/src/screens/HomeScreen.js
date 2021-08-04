ed28c238182ecf41caabe99028966642c20bfd4f
astevenaa <astevenaa@gmail.com>
2021-08-04 16:06:37 -0700

    added products props images

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,7 @@
+/* eslint-disable react/jsx-key */
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
 import products from "../products";
 const HomeScreen = () => {
   return (
@@ -7,9 +9,8 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          // eslint-disable-next-line react/jsx-key
           <Col sm={12} md={6} lg={4} lx={3}>
-            <h3>{product.name}</h3>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
