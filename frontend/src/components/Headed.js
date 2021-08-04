c8c2dfabfc748143674af84e0cab5dff9be8f7c1
astevenaa <astevenaa@gmail.com>
2021-08-03 22:43:49 -0700

    added nav bar and links in navbar

--- frontend/src/components/Headed.js
+++ frontend/src/components/Headed.js
@@ -6,12 +6,16 @@ const Headed = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="/">Pro-shop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/cart">Cart</Nav.Link>
-              <Nav.Link href="/login">Sign In</Nav.Link>
+            <Nav className="mr-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i>Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
