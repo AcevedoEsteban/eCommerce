d23e69c346cd3eda27854c337b9100d080a57054
astevenaa <astevenaa@gmail.com>
2021-08-03 19:49:16 -0700

    added nav bar and links in navbar

--- frontend/src/components/Headed.js
+++ frontend/src/components/Headed.js
@@ -1,34 +1,21 @@
 import React from "react";
-import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
 
 const Headed = () => {
   return (
     <header>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="/cart">Cart</Nav.Link>
+              <Nav.Link href="/login">Sign In</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
-      </Navbar>{" "}
+      </Navbar>
     </header>
   );
 };
