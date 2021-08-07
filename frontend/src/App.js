d6e9d52c24723267407fe276680105a7a252eb26
astevenaa <astevenaa@gmail.com>
2021-08-07 15:00:25 -0700

    added react-packes and paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Headed";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen></HomeScreen>
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
