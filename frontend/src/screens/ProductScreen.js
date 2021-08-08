51eee3d308e796504a5ef8c92da1b4b840be401b
astevenaa <astevenaa@gmail.com>
2021-08-07 17:44:34 -0700

    added link with router dom in order not to refresh the whole page

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+
+const ProductScreen = () => {
+  return <div>product</div>;
+};
+
+export default ProductScreen;
