940df5506693a57744deb5965c9d427616604463
astevenaa <astevenaa@gmail.com>
2021-08-27 16:01:27 -0700

    started the routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+const router = express.Router();
+import Product from "../models/productModel";
+
+app.get("/", async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+app.get("/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+export default router;
+b;
