9a48a782307767404a611521b0e116c64b24c83c
astevenaa <astevenaa@gmail.com>
2021-08-29 14:58:35 -0700

    middleware in server.js to send err code

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -23,9 +23,9 @@ router.get(
     const product = await Product.findById(req.params.id);
 
     if (product) {
-      return res.json(product);
+      res.json(product);
     } else {
-      res.status(404).json({ message: "Product not founder" });
+      res.status(404).json({ message: "Product not found" });
     }
   })
 );
