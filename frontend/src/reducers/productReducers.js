bc76d53a80df6982e34409d05df13f248b01ee6d
astevenaa <astevenaa@gmail.com>
2021-09-01 15:26:45 -0700

    product detail connected to productScreen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -26,9 +26,9 @@ export const productDetailsReducer = (
 ) => {
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
-      return { loading: true, products: [] };
+      return { loading: true, ...state };
     case PRODUCT_DETAILS_SUCCESS:
-      return { loading: false, products: action.payload };
+      return { loading: false, product: action.payload };
     case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
