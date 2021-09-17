8dd8214b545fcc5ce1637d06171bb930bde0131d
astevenaa <astevenaa@gmail.com>
2021-09-17 14:42:28 -0700

    add user through backend routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,39 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Register a new user
+// @route Post /api/users/
+// @access public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExsists = await User.findOne({ email });
+
+  if (userExsists) {
+    res.status(404);
+    throw new Error("user already exsists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    //201 status means something was created
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
 // @desc Get user profile
 // @route GET /api/users/login
 // @access Private
@@ -42,4 +75,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
