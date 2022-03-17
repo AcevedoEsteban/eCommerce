d5756a620e45dfa3f526d4a8f45e09ac6d048acd
astevenaa <astevenaa@gmail.com>
2022-03-16 20:26:29 -0700

    login screen

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,4 +1,8 @@
-import React, {userState, useEffect} from 'react'
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import {Form, Button, Row, Col} from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
@@ -7,14 +11,21 @@ import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
 import { login } from '../actions/userActions'
 
-const LoginScreen = () => {
-const [email, setEmail] = useState('')
+
+const LoginScreen = ({ location }) => {
+const [email, setEmail] = useState(' ')
 const [password, setPassword] = useState('')
 
+const redirect= location.search ? location.search.split('=')[1] : '/'
+
+const submitHandler = (e) => {
+    e.preventDefault()
+    //dispatch login
+}
+
   return (
-    <><FormContainer>
+    <FormContainer>
           <h1>Sign In</h1>
-      </FormContainer>
       <Form onSubmit={submitHandler}>
               <Form.Group controlId='email'>
                   <Form.Label>Email Address</Form.Label>
@@ -31,7 +42,15 @@ const [password, setPassword] = useState('')
                   onChange={(e) => setPassword(e.target.value)}
                   ></Form.Control>
               </Form.Group>
-          </Form></>
+              <Button type='submit' variant='primary'>
+                  Sign In </Button>
+          </Form>
+          <Row className='py-3'>
+              <Col>
+              New Here? create account here <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}> Register</Link> 
+              </Col>
+               </Row>
+                  </FormContainer>
   )
 }
 
