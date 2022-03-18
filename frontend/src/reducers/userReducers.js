0bf24ebd7b7eeaf789ac11c94365cd03ae5e7f62
astevenaa <astevenaa@gmail.com>
2022-03-17 22:29:51 -0700

    register

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,6 +3,7 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
@@ -23,7 +24,7 @@ export const userLoginReducer = (state = {}, action) => {
   }
 };
 
-export const useRegisterReducer = (state = {}, action) => {
+export const userRegisterReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_REGISTER_REQUEST:
       return { loading: true };
