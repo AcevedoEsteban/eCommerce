3d7a8fd3f3702dec8d3bebcdc1d4d51c988f5e25
astevenaa <astevenaa@gmail.com>
2022-03-27 18:28:35 -0700

    payment options reducers and actions

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -4,85 +4,53 @@ import { Form, Button, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
 import CheckoutSteps from "../components/CheckoutSteps";
-import { saveShippingAddress } from "../actions/cartActions";
+import { savePaymentMethod } from "../actions/cartActions";
 
-// eslint-disable-next-line no-unused-vars
-const ShippingScreen = ({ history }) => {
+const PaymentScreen = ({ history }) => {
   const cart = useSelector((state) => state.cart);
   const { shippingAddress } = cart;
 
-  const [address, setAddress] = useState(shippingAddress.address);
-  const [city, setCity] = useState(shippingAddress.city);
-  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
-  const [country, setCountry] = useState(shippingAddress.country);
+  if (!shippingAddress) {
+    history.push("/shipping");
+  }
+  const [paymentMethod, setPaymentMethod] = useState("Paypal");
 
   const dispatch = useDispatch();
 
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(saveShippingAddress({ address, city, postalCode, country }));
-    history.push("/payment");
+    dispatch(savePaymentMethod({ paymentMethod }));
+    history.push("/placeorder");
   };
 
   return (
     <FormContainer>
-      <CheckoutSteps step1 step2 />
-      <h1>Shipping</h1>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
       <Form onSubmit={submitHandler}>
-        <Form.Group controlId="address">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="enter address"
-            value={address}
-            required
-            onChange={(e) => setAddress(e.target.value)}
-          ></Form.Control>
+        <Form.Group>
+          <Form.Label as="legend">Select Method</Form.Label>
         </Form.Group>
+        <Col>
+          <Form.Check
+            tpe="radio"
+            label="Paypal or Credit Card"
+            id="Paypal"
+            name="paymentMethod"
+            value="Paypal"
+            checked
+            onChange={(e) => setPaymentMethod(e.targert.value)}
+          ></Form.Check>
 
-        <Form.Group controlId="city">
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="enter city"
-            value={city}
-            required
-            onChange={(e) => setCity(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-
-        <Form.Group controlId="postalCode">
-          <Form.Label>PostalCode</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="enter postal code"
-            value={postalCode}
-            required
-            onChange={(e) => setPostalCode(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-
-        <Form.Group controlId="city">
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="enter city"
-            value={city}
-            required
-            onChange={(e) => setCity(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-
-        <Form.Group controlId="country">
-          <Form.Label>Country</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="enter country"
-            value={country}
-            required
-            onChange={(e) => setCountry(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+          <Form.Check
+            tpe="radio"
+            label="Stripe"
+            id="Stripe"
+            name="paymentMethod"
+            value="Stripe"
+            onChange={(e) => setPaymentMethod(e.targert.value)}
+          ></Form.Check>
+        </Col>
         <Button type="submit" variant="primary">
           Continue
         </Button>
@@ -91,4 +59,4 @@ const ShippingScreen = ({ history }) => {
   );
 };
 
-export default ShippingScreen;
+export default PaymentScreen;
