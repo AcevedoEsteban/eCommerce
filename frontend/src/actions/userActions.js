d9bd2694a861432d305bbebd65aafee9f4aed502
astevenaa <astevenaa@gmail.com>
2022-04-21 23:55:19 -0700

    fixed clear state for empting out orders when loggining into diffrent profile

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
 import {
   USER_DETAILS_FAIL,
   USER_DETAILS_REQUEST,
@@ -13,8 +14,8 @@ import {
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
+  USER_DETAILS_RESET,
 } from "../constants/userConstants";
-
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
@@ -52,6 +53,8 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
+  dispatch({ type: USER_DETAILS_RESET });
+  dispatch({ type: ORDER_LIST_MY_RESET });
 };
 
 export const register = (name, email, password) => async (dispatch) => {
