912bb7d4c7d59940d162082d6f85337e448cf4b3
astevenaa <astevenaa@gmail.com>
2022-05-02 22:49:23 -0700

    fixed server.js file

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -24,11 +24,11 @@ const HomeScreen = () => {
         <Message variant="danger">{error}</Message>
       ) : (
         <Row>
-          {products.map((product) => (
+          {products.map((product) => {
             <Col key={product._id} sm={12} md={6} lg={4} lx={3}>
               <Product product={product} />
-            </Col>
-          ))}
+            </Col>;
+          })}
         </Row>
       )}
     </>
