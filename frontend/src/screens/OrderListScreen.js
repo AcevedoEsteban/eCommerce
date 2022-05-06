1eb51b4ef938998ecebd0f8f073ce100430dc590
astevenaa <astevenaa@gmail.com>
2022-05-05 23:17:58 -0700

    fix view details for order list

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -85,7 +85,7 @@ const OrderListScreen = ({ history }) => {
                     )}
                   </td>
                   <td>
-                    <LinkContainer to={`/admin/order/${order._id}`}>
+                    <LinkContainer to={`/order/${order._id}`}>
                       <Button variant="light" className="btn-sm">
                         <i className="fas fa-edit">Details</i>
                       </Button>
