61175dc7bbbad9eb271c00f2ecd3e01facde2c73
astevenaa <astevenaa@gmail.com>
2022-05-05 23:26:58 -0700

    backend for order delivery

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -93,10 +93,30 @@ const getOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({}).populate("user", "id name");
   res.json(orders);
 });
+
+// @desc    Update order to deliverery
+// @route   GET /api/orders/:id/delivery
+// @access  Private/admin
+const updateOrderDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliverdAt = Date.now();
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
 export {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
   getOrders,
+  updateOrderDelivered,
 };
