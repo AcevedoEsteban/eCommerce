895019389546b5ead6b87fefa25d0e30ec0a2575
astevenaa <astevenaa@gmail.com>
2022-05-06 23:19:37 -0700

    fixed product review contoller and route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -102,19 +102,19 @@ const createProductReview = asyncHandler(async (req, res) => {
       res.status(400);
       throw new Error("product already reviewed");
     }
-    const rerview = {
+    const reviews = {
       name: req.user.name,
       rating: Number(rating),
       comment,
       user: req.user._id,
     };
-    product.reviews.push(review);
+    product.reviews.push(reviews);
     //add another revirw to stars
     product.numReviews = product.reviews.length;
 
     product.rating =
       product.reviews.reduce((acc, item) => item.rating + acc, 0) /
-      products.reviews.length;
+      product.reviews.length;
 
     await product.save();
     res.status(201).json({ message: "Review added" });
