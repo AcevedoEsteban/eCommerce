342d230b940c22f12daa875a2211880e3f46caf2
astevenaa <astevenaa@gmail.com>
2022-05-07 23:23:57 -0700

    added search box

--- frontend/src/components/Headed.js
+++ frontend/src/components/Headed.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
 import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import SearchBox from "./SearchBox";
 import { logout } from "../actions/userActions";
 
 const Headed = () => {
@@ -33,7 +34,8 @@ const Headed = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
+            <SearchBox></SearchBox>
+            <Nav className="ml-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <i className="fas fa-shopping-cart"></i> Cart
